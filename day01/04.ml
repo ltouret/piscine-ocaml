66a941f36bc080019ed3eb3be8bfe16be6233f0c
LTouret <leonard.touret@fabernovel.ey.com>
2026-02-06 23:38:50 +0100

    done with d01, gogogo!

--- day01/04.ml
+++ day01/04.ml
@@ -1,3 +1,32 @@
 let fibonacci n =
+    if n < 0 then
+        -1
+    else (
+        let rec aux n curr next =
+            if n = 0 then
+                curr
+            else 
+                aux (n - 1) (next) (curr + next)
+        in
+        aux n 0 1
+    )
+
 
 let _ =
+        print_int(fibonacci (-1));
+        print_char '\n';
+        print_int(fibonacci 0);
+        print_char '\n';
+        print_int(fibonacci 1);
+        print_char '\n';
+        print_int(fibonacci 2);
+        print_char '\n';
+        print_int(fibonacci 3);
+        print_char '\n';
+        print_int(fibonacci 4);
+        print_char '\n';
+        print_int(fibonacci 5);
+        print_char '\n';
+        print_int(fibonacci 6);
+        print_char '\n';
+        print_int(fibonacci 11);
