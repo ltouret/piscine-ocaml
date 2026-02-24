e37d097bd722e61258001f0df0abd1a5e9653f95
Leonard TOURET <ltouret@f6r12s12.clusters.42paris.fr>
2026-02-24 20:01:21 +0100

    wip

--- d07/ex04/alkane_combustion.ml
+++ d07/ex04/alkane_combustion.ml
@@ -1,8 +1,16 @@
 (* class virtual reaction i o = *)
+(*  *)
 class alkane_combustion (alkanes: Alkane.alkane list) =
 object (self)
+  (* first deduplicate the elems in alkanes list, then *)
   inherit Reaction.reaction 
   
   (* val start = start_list *)
   (* val result = result_list *)
+
+  (* get_start *)
+  (* get_result *)
+  (* balance *)
+  (* is_balanced *)
+
 end
