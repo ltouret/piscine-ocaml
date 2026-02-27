ce5e17e12797d79649baf9231f9c136d27834e4a
LT <leonard.touret@fabernovel.ey.com>
2026-02-27 22:13:54 +0100

    done

--- day03/ex02/main.ml
+++ day03/ex02/main.ml
@@ -1,41 +1,29 @@
 let () =
   print_endline "--- Testing Individual Cards ---";
-  let c1 = Card.newCard Value.As Color.Spade in
-  let c2 = Card.newCard Value.T2 Color.Heart in
-  let c3 = Card.newCard Value.King Color.Spade in
+  let c1 = Card.newCard Card.Value.As Card.Color.Spade in
+  let c2 = Card.newCard Card.Value.T2 Card.Color.Heart in
+  let c3 = Card.newCard Card.Value.King Card.Color.Spade in
 
-  print_endline ("Card 1: " ^ toStringVerbose c1);
-  print_endline ("Card 2: " ^ toString c2);
+  print_endline ("Card 1: " ^ Card.toStringVerbose c1);
+  print_endline ("Card 2: " ^ Card.toString c2);
   
   print_endline "\n--- Testing Comparison ---";
-  let winner = max c1 c2 in
-  print_endline ("Between Ace and 2, the winner is: " ^ toString winner);
+  let winner = Card.max c1 c2 in
+  print_endline ("Between Ace and 2, the winner is: " ^ Card.toString winner);
 
   print_endline "\n--- Testing Suit Filters ---";
-  let spade_check = isSpade c1 in
-  let heart_check = isSpade c2 in
-  Printf.printf "Is %s a Spade? %b\n" (toString c1) spade_check;
-  Printf.printf "Is %s a Spade? %b\n" (toString c2) heart_check;
+  let spade_check = Card.isSpade c1 in
+  let heart_check = Card.isSpade c2 in
+  Printf.printf "Is %s a Spade? %b\n" (Card.toString c1) spade_check;
+  Printf.printf "Is %s a Spade? %b\n" (Card.toString c2) heart_check;
 
   print_endline "\n--- Testing List Operations ---";
-  let hand = [c2; c3; Card.newCard Value.T10 Color.Diamond] in
+  let hand = [c2; c3; Card.newCard Card.Value.T10 Card.Color.Diamond] in
   print_endline "In the hand [2H, KS, 10D]:";
-  let strongest = best hand in
-  print_endline ("The best card is: " ^ toStringVerbose strongest);
+  let strongest = Card.best hand in
+  print_endline ("The best card is: " ^ Card.toStringVerbose strongest);
 
   print_endline "\n--- Testing The Full Deck ---";
-  let deck_size = List.length all in
-  print_endline ("Total cards in deck: " ^ string_of_int deck_size);
+  let deck_size = List.length Card.all in
+  print_endline ("Total cards in deck: " ^ string_of_int deck_size)
   
-  (* Print the first 5 cards of the deck as a sample *)
-  print_endline "First 5 cards in the generated deck:";
-  let rec print_sample count list =
-    match count, list with
-    | 0, _ | _, [] -> ()
-    | n, h::t -> 
-        print_endline (" - " ^ toString h); 
-        print_sample (n-1) t
-  in
-  print_sample 5 all;
-
-  print_endline "\nTests completed successfully!"
